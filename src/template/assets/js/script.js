7f326607bb05dd7f705e405417326140012d2e37
pasha <fastist@gmail.com>
2017-09-04 11:04:49 -0600

    add calendar

--- src/template/assets/js/script.js
+++ src/template/assets/js/script.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
     menu: '#main-menu',
     verticalCentered: false,
     autoScrolling: false,
-    //fitToSection: false,
+    fitToSection: false,
     onLeave: function(anchorLink, index){
       if(index == 1 || index == 7){
 
@@ -279,7 +279,80 @@ $(document).ready(function() {
     pagerType: 'short',
     nextText: '',
     prevText: ''
+  });
 
+
+
+  $('#calendar').fullCalendar({
+    locale: 'ru',
+    header: {
+      left:   'title',
+      center: '',
+      right:  'prev,next',
+    },
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2017-09-01'
+      },
+      {
+        title: 'Long Event',
+        start: '2017-09-07',
+        end: '2017-09-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2017-09-09T16:00:00'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2017-09-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2017-09-11',
+        end: '2017-09-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2017-09-12T10:30:00',
+        end: '2017-09-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2017-09-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2017-09-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2017-09-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2017-09-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2017-09-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2017-09-28'
+      }
+    ],
+    windowResize: function(view) {
+      if ($(window).width() < 514){
+        $('#calendar').fullCalendar( 'changeView', 'basicDay' );
+      } else {
+        $('#calendar').fullCalendar( 'changeView', 'month' );
+      }
+    }
   });
 
 
